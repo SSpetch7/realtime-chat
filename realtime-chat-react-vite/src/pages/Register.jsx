5dd63540d4f979d53c06d2a9869c7f04508faebd
Kittinan Samakpong <joker.use.3@gmail.com>
2023-08-01 03:14:13 +0700

    firebase

--- realtime-chat-react-vite/src/pages/Register.jsx
+++ realtime-chat-react-vite/src/pages/Register.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import addImg from '../img/add-image.png';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
 
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
   return (
     <div className="formContainer bg-orange-300 h-screen flex justify-center items-center">
       <div className="formWrapper bg-white px-16 py-14 rounded-md gap-5 text-center w-3/12 min-w-96 flex flex-col">
         <span className="text-orange-500 text-2xl font-bold ">ZipZip Chat</span>
         <span className="text-orange-500 text-base ">สมัครใช้งาน</span>
 
-        <form className="flex flex-col gap-4 ">
+        <form className="flex flex-col gap-4 " onSubmit={handleSubmit}>
           <input
             className="p-3 border-b border-orange-300"
             type="text"
